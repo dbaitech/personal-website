3c6310303a59b12d29218264d20498e8027abadc
dbaitech <diana.brebeanu@gmail.com>
2023-06-08 18:52:34 -0400

    Added basic NavBar

---
+++ my-website/src/components/Header/Header.js
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import { LinkContainer } from "react-router-bootstrap";
+
+function Header() {
+  return (
+    <Navbar bg="light" expand="lg">
+      <Container>
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <LinkContainer to="/">
+              <Nav.Link>Home</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/projects">
+              <Nav.Link>Projects</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/music">
+              <Nav.Link>Music</Nav.Link>
+            </LinkContainer>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default Header;
--- my-website/src/index.js
+++ my-website/src/index.js
@@ -1,11 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header/Header";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    <Router>
+      <Header />
+      <Routes>
+        <Route exact path="/" component={App} />
+        <Route path="/projects" component={App} />
+        <Route path="/music" component={App} />
+      </Routes>
+    </Router>
     <App />
   </React.StrictMode>
 );
