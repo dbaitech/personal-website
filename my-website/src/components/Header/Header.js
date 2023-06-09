2976fc4bf958a08803254c59602619c2208b0171
dbaitech <diana.brebeanu@gmail.com>
2023-06-08 20:31:31 -0400

    Added some navbar styling

--- my-website/src/components/Header/Header.js
+++ my-website/src/components/Header/Header.js
@@ -1,23 +1,56 @@
 import React, { useState } from "react";
 import Container from "react-bootstrap/Container";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
+import { Nav, Navbar } from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./Header.css";
 
 function Header() {
+  const [activeLink, setActiveLink] = useState("home");
+
+  const updateActiveLink = (value) => {
+    setActiveLink(value);
+  };
+
   return (
-    <Navbar bg="light" expand="lg">
+    <Navbar bg="light" expand="lg" className={"header"}>
       <Container>
+        <LinkContainer to="/">
+          <Navbar.Brand
+            className={
+              activeLink === "projects" ? "active navbar-link" : "navbar-link"
+            }
+            onClick={() => updateActiveLink("home")}
+          >
+            Home
+          </Navbar.Brand>
+        </LinkContainer>
+        <Navbar.Toggle aria-controls="basic-navbar-nav">
+          <span className="navbar-toggler-icon"></span>
+        </Navbar.Toggle>
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <LinkContainer to="/">
-              <Nav.Link>Home</Nav.Link>
-            </LinkContainer>
             <LinkContainer to="/projects">
-              <Nav.Link>Projects</Nav.Link>
+              <Nav.Link
+                className={
+                  activeLink === "projects"
+                    ? "active navbar-link"
+                    : "navbar-link"
+                }
+                onClick={() => updateActiveLink("projects")}
+              >
+                Projects
+              </Nav.Link>
             </LinkContainer>
             <LinkContainer to="/music">
-              <Nav.Link>Music</Nav.Link>
+              <Nav.Link
+                className={
+                  activeLink === "music" ? "active navbar-link" : "navbar-link"
+                }
+                onClick={() => updateActiveLink("music")}
+              >
+                Music
+              </Nav.Link>
             </LinkContainer>
           </Nav>
         </Navbar.Collapse>
