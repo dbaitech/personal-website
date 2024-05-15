0c24a5c18be13db8a553acee1c496ac559c97096
dbaitech <diana.brebeanu@gmail.com>
2024-05-15 14:48:19 -0400

    Using HashRouter

--- my-website/src/App.js
+++ my-website/src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./pages/Home";
 import About from "./pages/About";
@@ -10,18 +10,16 @@ import ScrollToTop from "./utils/ScrollToTop";
 
 function App() {
   return (
-    <React.StrictMode>
-      <Router>
-        <ScrollToTop />
-        <Header />
-        <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/resume" element={<Resume />} />
-        </Routes>
-      </Router>
-    </React.StrictMode>
+    <>
+      <ScrollToTop />
+      <Header />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/resume" element={<Resume />} />
+      </Routes>
+    </>
   );
 }
 
